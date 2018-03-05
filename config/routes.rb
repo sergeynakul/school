0ae6247c519220f6a7b112852cc2553b97b9574e
Sergey <nakul.sv@gmail.com>
2018-03-05 11:22:28 +0200

    connect bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+  	root to: 'main#index'
+    get 'main/index'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
